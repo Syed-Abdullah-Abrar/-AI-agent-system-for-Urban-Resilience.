e50e73b76a6a09fe38db4a4311607aa307ee8b22
Syed-Abdullah-Abrar <syedabdullahabrar5020@gmail.com>
2025-08-10 22:21:13 +0530

    Updating Agents

--- Risk_prediction/risk_prediction.py
+++ Risk_prediction/risk_prediction.py
@@ -1,7 +1,24 @@
 import os
 from typing import Annotated,TypedDict, List, Dict, Any, Optional
+from langchain_ibm import ChatWatsonx
+from langchain_core.messages import HumanMessage
 from langgraph.graph import StateGraph, START, END
 from langfuse import Langfuse
+
+parameters = {
+    "decoding_method": "greedy",
+    "max_new_tokens": 800,
+    "repetition_penalty": 1,
+    "min_new_tokens":1
+}
+
+Watson_llm = ChatWatsonx(
+    model_id="ibm/granite-34b-code-instruct",
+    url="https://us-south.ml.cloud.ibm.com",
+    apikey="YOUR API KEY",
+    project_id="YOUR PROJECT ID",
+    params=parameters,
+)
 
 langfuse = Langfuse(
   secret_key="sk-lf-f7b63763-fee5-46b9-9d93-d26f9aca1f67",
